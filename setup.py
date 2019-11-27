6846365c414bb15df47e9042d545e8e81e041ab8
Vuown <bhuwanaryal19@gmail.com>
2019-11-27 14:11:13 +0545

    Update version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/everestial/vcfparser',
-    version='0.1.9',
+    version='0.1.15',
     zip_safe=False,
 )
 
