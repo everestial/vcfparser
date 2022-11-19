4e2cdef30b30ddd1d850a23f245b962fc552871a
AchyutBurlakoti <heroraj14@gmail.com>
2022-11-19 17:48:03 +0545

    offset.txt added for 100mb vcf file

--- experimentation/experiment0/off.py
+++ experimentation/experiment0/off.py
@@ -1,8 +1,8 @@
 
-f = open("3gb.vcf", "r")
+f = open("100mb.vcf", "r")
 
 # You can copy any offset from offset.txt file to get to that position directly without linear probing 
-f.seek(105528005)
+f.seek(61846821)
 for line in f :
     print(line)
     break
